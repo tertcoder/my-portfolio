573ac5cd511fc16961e71dd96b9ac444c814f09b
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2023-11-04 14:28:10 +0200

    Update Works.tsx

--- src/pages/Works.tsx
+++ src/pages/Works.tsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function Works() {
   return (
-    <div className="border-x border-x-secondaryBg">
+    <div className="border-x border-x-secondaryBg px-2">
       <Outlet />
     </div>
   );
